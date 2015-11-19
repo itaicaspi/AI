48fffda6f8c0f8518fb6b6716369e1192af88b64
itaicaspi <itaicaspi@gmail.com>
2015-11-19 20:08:17 +0200

    filled up map_statistics function in stats.py

--- HW1/stats.py
+++ HW1/stats.py
@@ -5,7 +5,9 @@ $ python stats.py
 
 from collections import namedtuple
 from ways import load_map_from_csv
-
+from numpy import mean
+from collections import Counter
+from ways.info import ROAD_TYPES
 
 # Q7:
 def map_statistics(roads):
@@ -13,14 +15,17 @@ def map_statistics(roads):
     You can edit this function as you wish'''
 
     Stat = namedtuple('Stat', ['max', 'min', 'avg'])
+    links = [j.links for j in roads.junctions()]
+    links_num = [len(j) for j in links]
+    links_length = [t.distance for l in links for t in l]
     return {
         'Number of junctions': len(roads.junctions()),
-        'Number of links': sum([len(j.links) for j in roads.junctions()]),
-        'Outgoing branching factor': Stat(max=None, min=None, avg=None),
-        'Link distance': Stat(max=None, min=None, avg=None),
+        'Number of links': sum(links_num),
+        'Outgoing branching factor': Stat(max=max(links_num), min=min(links_num), avg=mean(links_num)),
+        'Link distance': Stat(max=max(links_length), min=min(links_length), avg=mean(links_length)),
         # value should be a dictionary
         # mapping each road_info.TYPE to the no' of links of this type
-        'Link type histogram': None,  # tip: use collections.Counter
+        'Link type histogram': Counter(ROAD_TYPES[t.highway_type] for l in links for t in l),  # tip: use collections.Counter
     }
 
 
