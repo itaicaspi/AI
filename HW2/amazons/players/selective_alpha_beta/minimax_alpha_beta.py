8d51f348a5ba2c7859c6587e29bd325ca3d0f353
itaicaspi <itaicaspi@gmail.com>
2015-12-13 21:21:59 +0200

    started working on a filter function for SelectiveAlphaBeta

--- HW2/amazons/players/selective_alpha_beta/minimax_alpha_beta.py
+++ HW2/amazons/players/selective_alpha_beta/minimax_alpha_beta.py
@@ -1,5 +1,15 @@
+# from __future__ import print_function
+from threading import Thread
+from queue import Queue
+import time
+import copy
+import math
+import numpy
+from operator import itemgetter
+
 __author__ = 'Orenk'
 
+INFINITY = float(6000)
 
 class SelectiveMiniMaxWithAlphaBetaPruning:
 
@@ -15,6 +25,26 @@ class SelectiveMiniMaxWithAlphaBetaPruning:
         self.my_color = my_color
         self.no_more_time = no_more_time
         self.w = w
+
+    # counts the reachable positions on the board
+    def filter_moves(self, state, moves, w):
+        sorted_moves = []
+        # go over all queen moves
+        for move in moves:  # move = ((a,b), (c,d), (e,f))
+            board = numpy.empty((8, 8))
+            counter = 0
+            # for each move, look at the following moves
+            queen_moves = state.getMoves(move(1))  # (a,b)
+            for qm in queen_moves:
+                x, y = qm(1)
+                if board[x][y] == 0:
+                    counter += 1
+                    board[x][y] = 1
+            sorted_moves += (move, counter)
+        sorted_moves = sorted(sorted_moves, key=itemgetter(1, 2))
+        total_moves = len(sorted_moves)
+        return sorted_moves(range(0, (w*total_moves)))
+
 
     def search(self, state, depth, alpha, beta, maximizing_player):
         """Start the MiniMax algorithm.
@@ -33,6 +63,8 @@ class SelectiveMiniMaxWithAlphaBetaPruning:
         if not next_moves:
             # This player has no moves. So the previous player is the winner.
             return INFINITY if state.currPlayer != self.my_color else -INFINITY, None
+
+        filtered_moves = self.filter_moves(next_moves, self.w)
 
         if maximizing_player:
             selected_move = next_moves[0]
