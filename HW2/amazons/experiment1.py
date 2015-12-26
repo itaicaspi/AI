951283eb41373cfabc1276c64e61bdda1aa98c64
peterKlepperbaz <ooren.kleinn@gmail.com>
2015-12-26 19:32:06 +0200

    updated experiment1

--- HW2/amazons/experiment1.py
+++ HW2/amazons/experiment1.py
@@ -2,28 +2,38 @@ from run_amazons import amazonsRunner
 
 players_scores = dict()
 players = ['simple_player', 'selective_alpha_beta_0_5', 'selective_alpha_beta_0_25', 'selective_alpha_beta_0_125']
-
+f = open('exp1_res.txt', 'w')
 for player in players:
     players_scores[player] = 0
 
 for k in [2, 10]:
+    f.write('k is' + str(k) + '\n')
     for player1 in players:
         for player2 in players:
             if player1 == player2:
                 continue
             winner_color = amazonsRunner(2, k, 5, 'n', player1, player2).run()
-            if winner_color == 'white':
+            winner = 'tie'
+            if winner_color[1] == 'white':
                 winner = player1
-            elif winner_color == 'black':
+                players_scores[winner] += 1
+            elif winner_color[1] == 'black':
                 winner = player2
+                players_scores[winner] += 1
 
-            players_scores[winner] += 1
-            print(k + ', ' + player1 + ', ' + player2 + ', ' + winner)
+            f.write('k: ' + str(k) + ', player1: ' + player1 + ', player2:' + player2 + ', winner:' + winner+ '\n')
+
             amazonsRunner(2, k, 5, 'n', player2, player1).run()
-            if winner_color == 'white':
+            if winner_color[1] == 'white':
                 winner = player1
-            elif winner_color == 'black':
+                players_scores[winner] += 1
+            elif winner_color[1] == 'black':
                 winner = player2
+                players_scores[winner] += 1
 
-            players_scores[winner] += 1
-            print(k + ', ' + player1 + ', ' + player2 + ', ' + winner)
+            f.write('k: ' + str(k) + ', player1: ' + player1 + ', player2:' + player2 + ', winner:' + winner+ '\n')
+
+for player in players:
+    f.write('player' + player + 'score is: ' + players_scores[player] + '\n')
+
+f.close()
