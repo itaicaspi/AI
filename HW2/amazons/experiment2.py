6187bc6214d61f7c9d3ca403f9eecdec234df1e1
itaicaspi <itaicaspi@gmail.com>
2015-12-31 16:01:16 +0200

    experiment 2 fix

--- HW2/amazons/experiment2.py
+++ HW2/amazons/experiment2.py
@@ -6,7 +6,7 @@ f = open('exp2_res_k2_10_50.txt', 'w')
 for player in players:
     players_scores[player] = 0
 
-for k in [10]:
+for k in [2, 10, 50]:
     f.write('k is' + str(k) + '\n')
     for player1 in players:
         for player2 in players:
